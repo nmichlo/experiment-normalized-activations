pseudo slope formula + fixes

exp/experiment.py
@@ -3,10 +3,10 @@ from typing import Optional
 from typing import Sequence
 from typing import Union
 
-import wandb
-from pytorch_lightning.loggers import WandbLogger
+import numpy as np
 import pytorch_lightning as pl
 import torch
+from pytorch_lightning.loggers import WandbLogger
 from torch import nn
 from torch.nn import functional as F
 
@@ -15,12 +15,12 @@ from exp.nn.model import get_ae_layer_sizes
 from exp.nn.model import make_normalized_model
 from exp.nn.model import norm_activations_analyse
 from exp.util.utils import plot_batch
+from exp.util.utils import print_stats
 
 
 # ========================================================================= #
 # Helper                                                                    #
 # ========================================================================= #
-from exp.util.utils import print_stats
 
 
 class SimpleSystem(pl.LightningModule):
@@ -134,7 +134,11 @@ class SimpleSystem(pl.LightningModule):
             max_epochs=epochs,
             weights_summary='full',
             checkpoint_callback=False,
-            logger=WandbLogger(name=wandb_name + re.sub(r'[\s|]+', '_', model.title).strip('_'), project='weight-init', tags=wandb_tags) if log_wandb else False,
+            logger=False if (not log_wandb) else WandbLogger(
+                name=(wandb_name + re.sub(r'[\s|]+', '_', model.title).strip('_')).lower(),
+                project='weight-init',
+                tags=wandb_tags
+            ),
             gpus=1 if torch.cuda.is_available() else 0,
         )
         trainer.fit(model, data)
@@ -147,8 +151,11 @@ class SimpleSystem(pl.LightningModule):
             # plot everything
             plot_batch(img_batch, out_batch, mean_std=data.norm_mean_std, clip=True)
         # print final stats
-        before_values, before_stats = norm_activations_analyse(model._base_model, obs_shape=[28*28], sampler=norm_sampler, steps=128, batch_size=32)
-        print_stats(before_stats, title=f'{model.title} | TRN')
+        train_values, train_stats = norm_activations_analyse(
+            model._base_model, obs_shape=[28 * 28], sampler=norm_sampler, steps=128, batch_size=32
+        )
+        print_stats(train_stats,  title=f'{model.title} | TRN STAT')
+        print_stats(train_values, title=f'{model.title} | TRN VALS')
         # FINISHED
         if log_wandb:
             import wandb
@@ -177,6 +184,33 @@ if __name__ == '__main__':
         wandb_name=WANDB_NAME,
         wandb_tags=WANDB_TAGS,
     )
+
+
+    # ===================================================================== #
+    # Generate Targets
+    # ===================================================================== #
+
+
+    def slope(a, n=len(LAYER_SIZES), y_0=0.001, y_n=1.0, zero_is_flat=False, tensor=True):
+        # check bounds
+        if not ((-1 < a < 0) or (0 < a < 1)):
+            raise ValueError('a must be in the range (-1, 0) or (0, 1)')
+        # scale slope values
+        if zero_is_flat:
+            # scale slope value -> 0 is flat
+            b = (1 / (1 - a)) if (a > 0) else (1 + a)
+        else:
+            # scale slope value -> 0 is sharp
+            b = (1 / a) if (a > 0) else (-a)
+        # compute values
+        x = np.arange(n, dtype='float32')
+        m = (y_n - y_0) / (b**(n - 1) - 1)
+        y = m * (b**x - 1) + y_0
+        # return values!
+        if tensor:
+            return torch.as_tensor(y, dtype=torch.float32)
+        return y
+
 
     # ===================================================================== #
     # TANH - BASE

exp/nn/model.py
@@ -98,8 +98,8 @@ def norm_activations_optimize(
                 targ_means, targ_stds = _make_targets_for(storage, means, stds, targets_mean, targets_std)
                 # compute loss
                 loss_mean = F.mse_loss(means, targ_means).mean()
-                loss_std = F.mse_loss(stds, targ_stds).mean()
-                loss = loss_mean + loss_std
+                loss_std  = F.mse_loss(stds, targ_stds).mean()
+                loss      = loss_mean + loss_std
                 # backpropagation
                 optimizer.zero_grad()
                 loss.backward()
@@ -167,15 +167,12 @@ def make_sequential_nn(
     layers = []
     for i, (inp, out) in enumerate(iter_pairs(sizes)):
         layers.append(nn.Linear(inp, out))
-        print(f'layer: {i} {inp} {out}')
         # dont add activation to last layer
         if (i < len(sizes) - 2):
             layers.append(activations(i, inp, out))
-            print(f'  - activation: {i} {inp} {out}')
             # add batch norm every nth layer
             if batch_norm and (i % batch_norm == 0):
                 layers.append(nn.BatchNorm1d(out, momentum=0.05))
-                print(f'    + batch_norm: {i} {inp} {out}')
     # make model
     model = nn.Sequential(*layers)
     # initialise
@@ -251,9 +248,10 @@ def make_normalized_model(
     # print everything
     title = f'{min(model_sizes):3d} {max(model_sizes):3d} {(len(model_sizes) - 1) // 2:3d} | {model_activation:18s} | {norm_sampler:15s} | {model_init_mode:15s} | bn {str(model_batch_norm):5s}'
     if log:
-        print_stats(before_stats,               title=f'{title} | BFR')
-        print_stats(after_stats,                title=f'{title} | AFT')
-        print_stats(before_values-after_values, title=f'{title} | DIF', centers=(0.0, 0.0), min_radius=(None, None))
+        print_stats(before_stats,  title=f'{title} | BFR: STAT')
+        print_stats(after_stats,   title=f'{title} | AFT: STAT')
+        print_stats(before_values, title=f'{title} | BFR: VALS')
+        print_stats(after_values,  title=f'{title} | AFT: VALS')
         print()
     # return the  optimized model and stats
     if stats: